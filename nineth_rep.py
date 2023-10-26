09cb97a8398d52f1a2f25fbaeeaafdf7c78c4334
PoojaSable6 <poojasable9523@gmail.comgit config --global user.email poojasable9523@gmail.com>
2023-10-26 21:59:48 +0530

    This is default paramater nineth rep commit message

---
+++ nineth_rep.py
@@ -0,0 +1,7 @@
+def my_function(country = "INDIA"): #default value
+  print("I am from " + country)
+#default arguments
+my_function("Sweden")
+my_function("india2")
+my_function()
+my_function("Brazil")
